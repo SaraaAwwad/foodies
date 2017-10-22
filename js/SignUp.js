883f344485b2128eda5280207d983f617767276c
Sara Awwad <saraaawwad12@gmail.com>
2017-10-22 15:14:22 +0200

    Add Sign Up/ Sign In Validation!

--- js/SignUp.js
+++ js/SignUp.js
@@ -1,14 +1,209 @@
-var modal = document.getElementById('signUp');
+//sign up elements
+var e = document.getElementById("em");
+var pass = document.getElementById("pw");
+var reppass = document.getElementById("rpw");
+
+//sign in elements
+var eIn = document.getElementById("emIn");
+var pwIn = document.getElementById("pwIn");
+
+//checks validity when you leave the key
+pass.addEventListener("keyup", pwVal);
+pass.addEventListener("keyup", reppwVal);
+e.addEventListener("keyup", eVal);
+reppass.addEventListener("keyup", reppwVal);
+
+//log in validity
+eIn.addEventListener("keyup", eInVal);
+pwIn.addEventListener("keyup", pwInVal);
+
+//sign up submission
+document.getElementById("formSu").addEventListener('submit', checkUp);
+
+//sign in sub
+document.getElementById("formSi").addEventListener('submit', checkIn);
+
+document.getElementById("logM").addEventListener("click", logAcc);
+document.getElementById("logHref").addEventListener("click", logAcc);
 
 document.getElementById("createM").addEventListener("click", createAcc);
 document.getElementById("closeM").addEventListener("click", closeMod);
-document.getElementById("cancelM").addEventListener("click", closeMod);
+document.getElementById("closeSi").addEventListener("click", closeSi);
 
+
+var pw1="";
+var Evalid = false;
+var Pvalid = false;
+var Cvalid = false;
+
+var I_Evalid = false;
+var I_Pvalid = false;
+
+function logAcc(){
+	closeMod();
+	document.getElementById('signIn').style.display='block';
+}
+
+function closeSi(){
+	
+	document.getElementById('signIn').style.display='none';
+}
 
 function createAcc() {
     document.getElementById('signUp').style.display='block';
 }
 
 function closeMod(){
+	pass.value="";
+	pass.style.borderColor="grey";
+	pass.style.borderWidth="1px";
+	pass.style.backgroundColor="white";
+
+	e.value="";
+	e.style.borderColor="grey";
+	e.style.backgroundColor="white";
+	e.style.borderWidth="1px";
+
+	reppass.value="";
+	reppass.style.borderColor="grey";
+	reppass.style.borderWidth="1px";
+	reppass.style.backgroundColor="white";
+
+	var error = document.getElementById("error").innerHTML="";
+
 	document.getElementById('signUp').style.display='none';
+}
+
+
+function eInVal(){
+	var re = /\S+@\S+\.\S+/;
+	var x = eIn.value;
+
+	if(re.test(x) == false)
+	{	
+		//false entry;
+		eIn.style.borderWidth = "3px";
+		eIn.style.borderColor = "#ff4d4d";
+		eIn.style.backgroundColor = "#ffcccc";
+		I_Evalid= false;
+	}
+	else{
+		eIn.style.borderColor = "#99ff99";
+		eIn.style.backgroundColor = "#ccffcc";
+		I_Evalid=true;
+	}
+
+}
+
+function pwInVal(){
+
+	var x = pwIn.value;
+	pw1 = x;
+	
+	if(x.length < 4)
+	{
+		pwIn.style.borderWidth = "3px";
+		pwIn.style.borderColor = "#ff4d4d";
+		pwIn.style.backgroundColor = "#ffcccc";
+		I_Pvalid=false;
+	}
+	else{
+		pwIn.style.borderColor = "#99ff99";
+		pwIn.style.backgroundColor = "#ccffcc";
+		I_Pvalid=true;
+	}
+
+}
+
+
+function eVal(){
+
+	//very simple regEx for email validation
+	var re = /\S+@\S+\.\S+/;
+	var x = e.value;
+
+	if(re.test(x) == false)
+	{	
+		//false entry;
+		e.style.borderWidth = "3px";
+		e.style.borderColor = "#ff4d4d";
+		e.style.backgroundColor = "#ffcccc";
+		Evalid= false;
+	}
+	else{
+		e.style.borderColor = "#99ff99";
+		e.style.backgroundColor = "#ccffcc";
+		Evalid=true;
+	}
+}
+
+function pwVal(){
+
+	var x = pass.value;
+	pw1 = x;
+	
+	if(x.length < 4)
+	{
+		pass.style.borderWidth = "3px";
+		pass.style.borderColor = "#ff4d4d";
+		pass.style.backgroundColor = "#ffcccc";
+		Pvalid=false;
+	}
+	else{
+		pass.style.borderColor = "#99ff99";
+		pass.style.backgroundColor = "#ccffcc";
+		Pvalid=true;
+	}
+
+}
+
+function reppwVal(){
+
+	var x = reppass.value;
+		if(x!=pw1)
+	{
+		reppass.style.borderWidth = "3px";
+		reppass.style.borderColor = "#ff4d4d";
+		reppass.style.backgroundColor = "#ffcccc";
+		Cvalid=false;
+	}
+	else{
+		reppass.style.borderColor = "#99ff99";
+		reppass.style.backgroundColor = "#ccffcc";
+		Cvalid=true;
+	}	
+}
+
+function checkUp(evt){
+	if(Evalid && Pvalid && Cvalid){
+		alert("Congrats, Signed Up!");
+		return true;
+	}
+	else{
+		var error = document.getElementById("error");
+		error.style.color="red";
+		error.style.fontSize = "20px";
+		error.innerHTML = "Please check that all your inputs are valid! <br><br>";
+		evt.preventDefault();
+		return false;
+	}
+
+}
+
+function checkIn(evt){
+
+	//Should process to ensure user entry is found on database -- !
+	
+	if(I_Evalid && I_Pvalid){
+		alert("Congrats, Signed In!");
+		return true;
+	}
+	else{
+		var error = document.getElementById("errorSi");
+		error.style.color="red";
+		error.style.fontSize = "20px";
+		error.innerHTML = "Please check that all your inputs are valid! <br><br>";
+		evt.preventDefault();
+		return false;
+	}
 }
