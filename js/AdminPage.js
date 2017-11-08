8f990eb13711596d2f7d26408c22f30b0adfd596
Farah Hisham Ahmed Mohamed Soliman <farah1502451@miuegypt.edu.eg>
2017-11-08 23:37:33 +0200

    Admin Modification

--- js/AdminPage.js
+++ js/AdminPage.js
@@ -1,11 +1,9 @@
-
+var buttonaccr = document.getElementById("buttontoggle");
+buttonaccr.addEventListener("click",show);
 
 function show(){
-	var panel = document.getElementById("paneltoggle");
-	var button = document.getElementById("buttontoggle");
-	
-    //button.toggle("active");
-    //var panel = this.nextElementSibling;
+	var panel = document.getElementById("paneltoggle");	
+    
     if (panel.style.maxHeight){
       panel.style.maxHeight = null;
     } else {
