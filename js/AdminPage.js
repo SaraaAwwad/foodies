e40ae2c35b90f7216711bf25a752e6a914480b8a
Farah Hisham Ahmed Mohamed Soliman <farah1502451@miuegypt.edu.eg>
2017-11-09 04:09:43 +0200

    Admin Modification

--- js/AdminPage.js
+++ js/AdminPage.js
@@ -11,3 +11,31 @@ function show(){
     } 
   
 }
+
+var addrestbutton = document.getElementById("addrest");
+addrestbutton.addEventListener("click",addRest);
+function addRest() 
+{
+  window.location.href = "newrestaurant.html";
+}
+
+/* function update(id){
+    //Get contents off cell clicked
+    var content = document.getElementById(id).firstChild.nodeValue;
+    //Switch to text input field
+	var newcontent ="";
+    document.getElementById(id).innerHTML = "<input type = 'text' name = 'txtNewInput' id = 'txtNewInput' value = '" + content +"'/>";
+	
+  }
+  // IN TD
+   id = "txtFirstname" onclick = "update(this.id)"
+*/
+
+/*
+var deleting = document.getElementById("deletebutton");
+deleting.addEventListener("click",update);
+function update(id){
+	
+document.getElementsByClassName("highlight").deleteRow(1);
+}
+*/
