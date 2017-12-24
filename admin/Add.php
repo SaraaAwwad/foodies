ae07518b3dde736271d8d5cd3d5e1e0215d91249
Menna-Mohamed <mennamohamed69@yahoo.com>
2017-12-24 12:13:23 +0200

    nothin bardo haha

--- admin/Add.php
+++ admin/Add.php
@@ -6,6 +6,7 @@ $db_obj = new dbconnect;
 	
 
 
+		
 		#attribute in table restaurant
 		$r_name = trim($_POST['Name']);
 		$r_hotline = trim($_POST['Hotline']);
@@ -13,10 +14,20 @@ $db_obj = new dbconnect;
 		$r_delvtime = trim($_POST['DelvTime']);
 		$r_img = trim($_POST['Image']);
 		$r_adminid = trim($_POST['AdminID']);
-
 		
+		//drop down
+	// 	<option value="Ffth" id="Fifth">Fifth Settlement</option>
+	// <option value="Mdi" id="Maadi">Maadi</option>
+	// <option value="Nsr" id="Nasr">Nasr City</option>
+	// <option value="Hlp" id="Helio">Heliopolis</option>
+	// <option value="Sher" id="Sheraton">Sheraton</option>
+	// <option value="Rh" id="Rehab">Rehab</option>
+	// <option value="Oct" id="October">6 October</option>
+	// <option value="Dki" id="Dokki">Dokki</option>
+	// <option value="Obo" id="Obour">Obour</option>
+	// <option value="Mok" id="Mokattam">Mokattam</option>
 		
-
+		?
 		#attribute in table cuisine
 		$r_cuisine = trim($_POST['Type']);
 
@@ -31,13 +42,28 @@ $db_obj = new dbconnect;
 
 		$db_obj->add_area($a_id,$a_area);
 
-		
+
+
+	// 	if (isset($_POST["saver"]){
+
+	// 	$addrest= $_POST['restarea'];
+
+	// 	for($i=0; $i<count($addrest); $i++){
+	// 	//echo $p_name[$i].'<br/>';
+
+	// 	//mysql_query("INSERT INTO rest_area(addrest)VALUES ('$addrest[$i]') or die(mysql_error()") ;
+
+	// 	$db_obj->add_area();
+
+	// 	}
+	// }
+
 
 
 
 		// function add()
 		// {
-		// 	if(isset($_POST['formSubmit'])){
+		// 	if(isset($_POST['restarea'])){
 
 		// 		//for the multiple select
 		// 		$restareas = $_POST['restarea'];
@@ -57,7 +83,7 @@ $db_obj = new dbconnect;
 		$db_obj->disconnect();
 
 		header("Location: newrestaurant.php");
-}
+
 		
 				
 ?>
